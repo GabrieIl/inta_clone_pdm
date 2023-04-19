ef9ab8559f262dcbdcd8e9d098db33d49a84e886
GabrieIl <gabriel.assunsas@gmail.com>
2023-04-19 19:39:09 -0300

    initial commint

---
+++ lib/src/feature/home/view/widget/bubble_stories.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UserBubbleStories extends StatelessWidget {
+  const UserBubbleStories({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('BubbleStories', style: TextStyle(fontSize: 48))),
+    );
+  }
+}
